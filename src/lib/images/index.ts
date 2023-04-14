18ffb712db33a69067633bf5b404b7f82e16eef3
alexis-decker <alexis.decker.aa@gmail.com>
2023-04-15 06:04:52 +0800

    added `company` logo

--- src/lib/images/index.ts
+++ src/lib/images/index.ts
@@ -1,4 +1,5 @@
 import businessDecisions from './business-decisions.svg'
 import devProductivity from './dev-productivity.svg'
+import lombokDeveloper from './lombok-developer.png'
 
-export { businessDecisions, devProductivity }
+export { businessDecisions, devProductivity, lombokDeveloper }
