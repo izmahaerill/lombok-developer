a28721b1ccd604f863d81a6406d3ddc184fb9fb2
alexis-decker <alexis.decker.aa@gmail.com>
2023-04-16 05:52:44 +0800

    added `csp` configurations

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,12 @@ const config = {
   ],
 
   kit: {
+    csp: {
+      mode: 'hash',
+      directives: {
+        'script-src': ['self']
+      }
+    },
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
