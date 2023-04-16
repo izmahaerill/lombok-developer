379386ea8b626cbefc90481692a7d878bd72ecee
alexis-decker <alexis.decker.aa@gmail.com>
2023-04-17 02:43:21 +0800

    add dummy data

---
+++ src/lib/constants.ts
@@ -0,0 +1,26 @@
+export const serviceLists: App.ServiceLists[] = [
+  {
+    title: 'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, vitae.'
+  },
+  {
+    title: 'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, vitae.'
+  },
+  {
+    title: 'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, vitae.'
+  },
+  {
+    title: 'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, vitae.'
+  },
+  {
+    title: 'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, vitae.'
+  },
+  {
+    title: 'Web Development',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, vitae.'
+  }
+]
