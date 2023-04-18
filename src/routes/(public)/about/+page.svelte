dbcaf1dd54c64f41d9a423f5dae45145aeea94ee
alexis-decker <alexis.decker.aa@gmail.com>
2023-04-18 23:01:20 +0800

    formatting project files

--- src/routes/(public)/about/+page.svelte
+++ src/routes/(public)/about/+page.svelte
@@ -1,97 +1,100 @@
-<script>
-    import Iconify from '@iconify/svelte'
-    import { businessDecisions, devProductivity } from '$lib/images'
-
+<script lang="ts">
+  // import Iconify from '@iconify/svelte'
+  // import { businessDecisions, devProductivity } from '$lib/images'
 </script>
 
 <section>
-    <div class="hero min-h-screen" style="background-image: url(https://wp.w3layouts.com/workcore/wp-content/themes/workcore/assets/images/banner5.jpg);">
-        <div class="hero-overlay bg-opacity-60"></div>
-        <div class="hero-content text-center text-neutral-content">
-          <div class="max-w-md">
-            <h1 class="mb-5 text-5xl font-bold"> <span>Lombok </span>Developer</h1>
-            <p class="mb-5">The Unique Solutions for Your Business, The Best Solutions For Your Business</p>
-            <button class="btn btn-outline text-white">Get started</button>
-            <button class="btn btn-outline btn-success px-12">Home</button>
-
-
-          </div>
-        </div>
-      </div>
-</section>
-
-
-<section class="container mt-32 mb-20">
-    <div class="py-20">
-        <div class="flex-col md:flex lg:flex-row-reverse">
-            <img class=" rounded-sm shadow-2xl border-r-4 border-emerald-500" src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG9mZmljZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="">
-            <div class=" pl-2">
-                <!-- <iconify class="w-32 h-32" icon="material-symbols:home-app-logo"></iconify> -->
-                <h1 class="text-4xl font-semibold mt-10 lg:mt-2 mb-2 ">ABOUT US</h1>
-                <hr>
-                <p class="py-5">We are a web design and digital marketing company based in Lombok, Indonesia. We have been designing many websites since 2004.</p>
-                <button class="btn btn-outline btn-success">Read More</button>
-            </div>
-        </div>
-    </div>
-</section>
-
-
-<section class=" mb-20 container bg-gray-700 py-20">
-    <h1 class="text-3xl font-semibold text-center text-white">VISIONS & MISIONS</h1>
-    <p class="text-center text-white">Lombok Developer</p>
-    <div class="flex-col md:flex lg:flex-row mt-5">
-        <p class=" bg-white p-16 border-l-4 border-emerald-500 "><span class="font-semibold text-2xl">Vision</span>
-            Menjadi perusahaan Teknologi Informasi terkemuka di Indonesia pada umumnya dan Bali khususnya.
+  <div
+    class="hero min-h-screen"
+    style="background-image: url(https://wp.w3layouts.com/workcore/wp-content/themes/workcore/assets/images/banner5.jpg);">
+    <div class="hero-overlay bg-opacity-60" />
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold"><span>Lombok </span>Developer</h1>
+        <p class="mb-5">
+          The Unique Solutions for Your Business, The Best Solutions For Your Business
         </p>
-
-        <p class="max-w-lg bg-emerald-500 p-16 text-white border-r-4 border-white "><span class="font-semibold text-2xl">Misions</span>
-            Menjadi perusahaan Teknologi Informasi terdepan dalam mengembangkan kualitas layanan, kreativitas,
-             peningkatan kualitas layanan, cepat, akurat dan tepat dalam aspek teknologi informasi.
-        </p>
-    </div>
-</section>
-
-
-<section class="text-center mb-20">
-    <div class="stats">
-        <div class="flex-col md:flex lg:flex-row">
-
-            <div class="stat place-items-center">
-              <div class="stat-title">Downloads</div>
-              <div class="stat-value">31K</div>
-              <div class="stat-desc">From January 1st to February 1st</div>
-            </div>
-            
-            <div class="stat place-items-center">
-              <div class="stat-title">Users</div>
-              <div class="stat-value text-primary">4,200</div>
-              <div class="stat-desc text-primary">↗︎ 40 (2%)</div>
-            </div>
-            
-            <div class="stat place-items-center">
-              <div class="stat-title">New Registers</div>
-              <div class="stat-value">1,200</div>
-              <div class="stat-desc">↘︎ 90 (14%)</div>
-            </div>
-        </div>
-  
-        
+        <button class="btn-outline btn text-white">Get started</button>
+        <button class="btn-outline btn-success btn px-12">Home</button>
       </div>
+    </div>
+  </div>
 </section>
 
+<section class="container mb-20 mt-32">
+  <div class="py-20">
+    <div class="flex-col md:flex lg:flex-row-reverse">
+      <img
+        class=" rounded-sm border-r-4 border-emerald-500 shadow-2xl"
+        src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG9mZmljZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+        alt="" />
+      <div class=" pl-2">
+        <!-- <iconify class="w-32 h-32" icon="material-symbols:home-app-logo"></iconify> -->
+        <h1 class="mb-2 mt-10 text-4xl font-semibold lg:mt-2">ABOUT US</h1>
+        <hr />
+        <p class="py-5">
+          We are a web design and digital marketing company based in Lombok, Indonesia. We have been
+          designing many websites since 2004.
+        </p>
+        <button class="btn-outline btn-success btn">Read More</button>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class=" container mb-20 bg-gray-700 py-20">
+  <h1 class="text-center text-3xl font-semibold text-white">VISIONS & MISIONS</h1>
+  <p class="text-center text-white">Lombok Developer</p>
+  <div class="mt-5 flex-col md:flex lg:flex-row">
+    <p class=" border-l-4 border-emerald-500 bg-white p-16">
+      <span class="text-2xl font-semibold">Vision</span>
+      Menjadi perusahaan Teknologi Informasi terkemuka di Indonesia pada umumnya dan Bali khususnya.
+    </p>
+
+    <p class="max-w-lg border-r-4 border-white bg-emerald-500 p-16 text-white">
+      <span class="text-2xl font-semibold">Misions</span>
+      Menjadi perusahaan Teknologi Informasi terdepan dalam mengembangkan kualitas layanan, kreativitas,
+      peningkatan kualitas layanan, cepat, akurat dan tepat dalam aspek teknologi informasi.
+    </p>
+  </div>
+</section>
+
+<section class="mb-20 text-center">
+  <div class="stats">
+    <div class="flex-col md:flex lg:flex-row">
+      <div class="stat place-items-center">
+        <div class="stat-title">Downloads</div>
+        <div class="stat-value">31K</div>
+        <div class="stat-desc">From January 1st to February 1st</div>
+      </div>
+
+      <div class="stat place-items-center">
+        <div class="stat-title">Users</div>
+        <div class="stat-value text-primary">4,200</div>
+        <div class="stat-desc text-primary">↗︎ 40 (2%)</div>
+      </div>
+
+      <div class="stat place-items-center">
+        <div class="stat-title">New Registers</div>
+        <div class="stat-value">1,200</div>
+        <div class="stat-desc">↘︎ 90 (14%)</div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <section class="container mb-32 py-20">
-    <p class="font-semibold text-4xl text-center my-10">Our Creative Team.</p>
-    <div class="card m-auto w-64 bg-base-100 shadow-xl">
-        <figure><img src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="Man" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">UI UX Developer</h2>
-          <p>Hulami Haekal</p>
-          <div class="card-actions justify-end">
-          </div>
-        </div>
-      </div>
+  <p class="my-10 text-center text-4xl font-semibold">Our Creative Team.</p>
+  <div class="card m-auto w-64 bg-base-100 shadow-xl">
+    <figure>
+      <img
+        src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+        alt="Man" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">UI UX Developer</h2>
+      <p>Hulami Haekal</p>
+      <div class="card-actions justify-end" />
+    </div>
+  </div>
 </section>
-
-
