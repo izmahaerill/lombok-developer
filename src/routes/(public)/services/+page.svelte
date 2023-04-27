3a4cbc5b2ea9a476e4e874ca94b574d1c8c89e8a
haekal404 <hulaimihaikal@gamil.com>
2023-04-27 22:44:33 +0800

    update page service

--- src/routes/(public)/services/+page.svelte
+++ src/routes/(public)/services/+page.svelte
@@ -1,1 +1,33 @@
-services
+<script lang="ts">
+    import { base } from '$app/paths'
+    import SliderExample from '$lib/components/slider-example.svelte'
+    import { serviceLists } from '$lib/constants'
+    import { businessDecisions, devProductivity } from '$lib/images'
+    import Iconify from '@iconify/svelte'
+  
+  </script>
+
+
+
+
+  <section class="container py-40 flex flex-col  text-center">
+    <h2 class="mb-20 text-3xl font-bold md:text-4xl">
+      <span class="bg-gradient-to-l from-primary to-neutral bg-clip-text text-transparent">
+        Lombok Developer Services
+      </span>
+    </h2>
+    <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+      {#each serviceLists as service}
+        <div class="card rounded-sm h-62 w-62 shadow pt-4">
+          <figure class="px-5 pt-5">
+            <img class="h-fit w-fit" src={businessDecisions} alt="Business Decisions" />
+          </figure>
+          <div class="card-body items-center cursor-pointer">
+            <h2 class="card-title text-base font-semibold">{service.title}</h2>
+            <p>{service.description}</p>
+          </div>
+          <!-- <a class="btn btn-outline btn-success w-52 m-auto my-2 normal-case rounded-full" href={`${base}/service`}>Read More</a> -->
+        </div>
+      {/each}
+    </div>
+  </section>
