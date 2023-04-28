c7c9e4aab8b17b244321b045658c710aaf13b1b5
alexis-decker <alexis.decker.aa@gmail.com>
2023-04-28 22:14:48 +0800

    add `dummy` price card

---
+++ src/lib/components/home/price.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { base } from '$app/paths'
+</script>
+
+<div class="card w-60 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">PREMIUM</h2>
+    <p class="text-lg font-medium text-primary">Rp 500.000.00</p>
+    <div class="py-5">
+      <p>Gratis Domain</p>
+      <p>Max 10 Halaman</p>
+      <p>Hosting 2GB</p>
+    </div>
+    <a class="btn-outline btn-primary btn-block btn capitalize" href={`${base}/services`}>
+      Buy now
+    </a>
+  </div>
+</div>
