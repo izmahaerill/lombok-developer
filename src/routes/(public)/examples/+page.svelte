2c3e31e900158fd71ce7fea25547989af6ba6986
haekal404 <hulaimihaikal@gamil.com>
2023-05-07 21:57:10 +0800

    update again kwkkw

--- src/routes/(public)/examples/+page.svelte
+++ src/routes/(public)/examples/+page.svelte
@@ -1,1 +1,45 @@
-examples
+<section class="py-32 container">
+    <p class="mb-20 text-3xl font-bold text-center">Example Website</p>
+
+    <div class="flex-col gap-3 md:flex lg:flex-row justify-center">
+
+        <div class="card  glass mb-5">
+            <figure><img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdlYnNpdGUlMjBmb3Jmb2xpb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="car!"/></figure>
+            <div class="card-body">
+              <h2 class="card-title">Office Website</h2>
+              <p>How to park your car at your garage?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Learn now!</button>
+              </div>
+            </div>
+          </div>
+        <div class="card  glass mb-5">
+            <figure><img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdlYnNpdGUlMjBmb3Jmb2xpb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="car!"/></figure>
+            <div class="card-body">
+              <h2 class="card-title">Forfolio Website</h2>
+              <p>How to park your car at your garage?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Learn now!</button>
+              </div>
+            </div>
+          </div>
+        <div class="card glass mb-5">
+            <figure><img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdlYnNpdGUlMjBmb3Jmb2xpb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="car!"/></figure>
+            <div class="card-body">
+              <h2 class="card-title">Life hack</h2>
+              <p>How to park your car at your garage?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Learn now!</button>
+              </div>
+            </div>
+          </div>
+    </div>
+
+
+      
+
+      
+
+      
+      
+</section>
